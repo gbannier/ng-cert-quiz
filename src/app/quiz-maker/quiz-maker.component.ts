97e8b0748f61e8e6e19fa8dcdd61ec845c152723
gordonbannier <gordon.bannier@emineo.ch>
2023-08-17 12:30:51 +0200

    Filter maincategories and pass the correct id

--- src/app/quiz-maker/quiz-maker.component.ts
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -1,23 +1,73 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Category, Difficulty, Question} from '../data.models';
 import {Observable} from 'rxjs';
 import {QuizService} from '../quiz.service';
+import {BehaviorSubject} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz-maker',
   templateUrl: './quiz-maker.component.html',
   styleUrls: ['./quiz-maker.component.css']
 })
-export class QuizMakerComponent {
+export class QuizMakerComponent implements OnInit {
 
-  categories$: Observable<Category[]>;
-  questions$!: Observable<Question[]>;
 
-  constructor(protected quizService: QuizService) {
-    this.categories$ = quizService.getAllCategories()
+  categories$: Observable<Category[]>| undefined = undefined ;
+  subCategories$: BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
+  selectedCategoryId: null | undefined | string | number = null; // todo typing
+  showSubCategories = false;
+  questions$: Observable<Question[]>| undefined = undefined ;
+  mainCategories$: Observable<Category[]> | undefined = undefined;
+
+  constructor(private quizService: QuizService) {
+
   }
 
-  createQuiz(cat: string, difficulty: string): void {
-    this.questions$ = this.quizService.createQuiz(cat, difficulty as Difficulty);
+  ngOnInit() {
+    this.categories$ = this.quizService.getAllCategories();
+    this.mainCategories$ = this.categories$.pipe(
+      map(categories => {
+        const uniqueMainCategories: Category[] = [];
+        const seenNames = new Set<string>();
+        for (const category of categories) {
+          const mainCategoryName = category.name.split(':')[0];
+          if (!seenNames.has(mainCategoryName)) {
+            uniqueMainCategories.push({id: category.id, name: mainCategoryName});
+            seenNames.add(mainCategoryName);
+          }
+        }
+        return uniqueMainCategories;
+      })
+    );
   }
+
+  onMainCategoryChange(selectedMainCategoryName: string) {
+    this.categories$?.pipe(
+      map(categories => ({
+        mainCategory: categories.find(category => category.name.split(':')[0] === selectedMainCategoryName),
+        subCategories: categories.filter(category => category.name.startsWith(selectedMainCategoryName + ':'))
+      }))
+    ).subscribe(({mainCategory, subCategories}) => {
+      this.selectedCategoryId = mainCategory?.id;
+      this.showSubCategories = subCategories.length > 0;
+      this.subCategories$.next(subCategories);
+    });
+  }
+
+
+  onSubCategoryChange(selectedSubCategory: string) {
+    this.selectedCategoryId = selectedSubCategory;
+  }
+
+  createQuiz(difficulty: string) {
+    console.log(this.selectedCategoryId, difficulty)
+    if (this.selectedCategoryId) {
+      this.questions$ = this.quizService.createQuiz(this.selectedCategoryId.toString(), difficulty as Difficulty); // typing todo ls
+    }
+  }
+
+  protected readonly undefined = undefined;
 }
+
+
