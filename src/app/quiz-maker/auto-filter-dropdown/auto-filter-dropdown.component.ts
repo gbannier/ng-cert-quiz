617dbc27c08a7f4b7d36a408276f3a9c32fe3e4a
gordonbannier <gordon.bannier@emineo.ch>
2023-08-20 09:36:23 +0200

    cleanup

--- src/app/quiz-maker/auto-filter-dropdown/auto-filter-dropdown.component.ts
+++ src/app/quiz-maker/auto-filter-dropdown/auto-filter-dropdown.component.ts
@@ -1,65 +1,58 @@
-import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
-export interface DropdownOption {
-  value: string | number;
-  label: string;
-}
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {DropdownOption} from "./dropdown-option.model";
 @Component({
-  selector: 'app-auto-filter-dropdown',
-  templateUrl: './auto-filter-dropdown.component.html',
-  styleUrls: ['./auto-filter-dropdown.component.css']
+    selector: 'app-auto-filter-dropdown',
+    templateUrl: './auto-filter-dropdown.component.html',
+    styleUrls: ['./auto-filter-dropdown.component.css']
 })
-export class AutoFilterDropdownComponent implements OnInit{
-  @Input() options: DropdownOption[] = [];
-  @Input() labelModifierFn: ((label: string)=>string) | undefined = undefined;
-  @Input() placeholder: string = 'Select Item...';
-  @Output() selectionChange = new EventEmitter<DropdownOption>();
-  filterText = '';
-  filteredOptions: DropdownOption[] = [];
-  isDropdownOpen = false;
-  ngOnInit() {
-    if (this.options.length > 0 && this.labelModifierFn) {
-      this.modifyLabel();
-    }
-    this.filteredOptions = [...this.options];
+export class AutoFilterDropdownComponent implements OnInit {
+    @Input() options: DropdownOption[] = [];
+    @Input() labelModifierFn: ((label: string) => string) | undefined = undefined;
+    @Input() placeholder: string = 'Select Item...';
+    @Output() selectionChange = new EventEmitter<DropdownOption>();
+    filterText = '';
+    filteredOptions: DropdownOption[] = [];
+    isDropdownOpen = false;
 
-  }
-
-  private modifyLabel() {
-    this.options = this.options.map(option =>
-        ({
-          label: this.labelModifierFn!(option.label),
-          value: option.value
-        })
-    );
-  }
-  onInputFocus() {
-    this.isDropdownOpen = true;
-  }
-
-  onInputBlur() {
-    this.isDropdownOpen = false;
-  }
-  onFilterChange() {
-    if (this.options.length > 0 && this.labelModifierFn) {
-      this.modifyLabel();
-    }
-    this.filteredOptions = [...this.options];
-    if(this.filterText.length>1){
-      this.filteredOptions = this.options.filter(option =>
-          option.label.toLowerCase().includes(this.filterText.toLowerCase())
-      );
+    ngOnInit() {
+        this.prepareFilteredOptions();
     }
 
-  }
+    onInputFocus() {
+        this.isDropdownOpen = true;
+    }
 
-  onSelectChange(option: DropdownOption) {
-    console.log(option, 'onSelectChange')
-    this.filterText = option.label;
-    this.selectionChange.emit(option);
-    this.isDropdownOpen = false;
-  }
+    onFilterChange() {
+        this.prepareFilteredOptions();
+        if (this.filterText.length > 1) {
+            this.filteredOptions = this.options.filter(option =>
+                option.label.toLowerCase().includes(this.filterText.toLowerCase())
+            );
+        }
+    }
+
+    onSelectChange(option: DropdownOption) {
+        this.filterText = option.label;
+        this.selectionChange.emit(option);
+        this.isDropdownOpen = false;
+    }
+
+    private modifyLabel() {
+        this.options = this.options.map(option =>
+            ({
+                label: this.labelModifierFn!(option.label),
+                value: option.value
+            })
+        );
+    }
+
+    private prepareFilteredOptions() {
+        if (this.options.length > 0 && this.labelModifierFn) {
+            this.modifyLabel();
+        }
+        this.filteredOptions = [...this.options];
+    }
 }
-
 
 
 
--- src/app/quiz-maker/quiz-maker.component.ts
+++ src/app/quiz-maker/quiz-maker.component.ts
@@ -4,85 +4,84 @@ import {Observable, of, shareReplay} from 'rxjs';
 import {QuizService} from '../quiz.service';
 import {BehaviorSubject} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {DropdownOption} from "./auto-filter-dropdown/auto-filter-dropdown.component";
+import {DropdownOption} from "./auto-filter-dropdown/dropdown-option.model";
 
 @Component({
-  selector: 'app-quiz-maker',
-  templateUrl: './quiz-maker.component.html',
-  styleUrls: ['./quiz-maker.component.css']
+    selector: 'app-quiz-maker',
+    templateUrl: './quiz-maker.component.html',
+    styleUrls: ['./quiz-maker.component.css']
 })
 export class QuizMakerComponent implements OnInit {
 
 
-  categories$: Observable<Category[]>| undefined = undefined ;
-  subCategories$: BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
-  selectedCategoryId: null | undefined | string | number = null; // todo typing
-  showSubCategories = false;
-  questions$: Observable<Question[]>| undefined = undefined ;
-  mainCategories$: Observable<Category[]> = of([]);
-  labelModifierFn: ((label: string) => string) | undefined;
-  difficultyOptions: DropdownOption[]=[
-      {value:'Easy', label: 'Easy'},
-      {value:'Medium', label: 'Medium'},
-      {value:'Hard', label: 'Hard'}
-  ];
-  selectedDifficulty: Difficulty | undefined = undefined;
+    categories$: Observable<Category[]> | undefined = undefined;
+    subCategories$: BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
+    selectedCategoryId: null | undefined | string | number = null; // todo typing
+    showSubCategories = false;
+    questions$: Observable<Question[]> | undefined = undefined;
+    mainCategories$: Observable<Category[]> = of([]);
+    labelModifierFn: ((label: string) => string) | undefined;
+    difficultyOptions: DropdownOption[] = [
+        {value: 'Easy', label: 'Easy'},
+        {value: 'Medium', label: 'Medium'},
+        {value: 'Hard', label: 'Hard'}
+    ];
+    selectedDifficulty: Difficulty | undefined = undefined;
 
-  constructor(private quizService: QuizService, private changeDetectorRef: ChangeDetectorRef) {
-
-
-  }
-
-  ngOnInit() {
-    this.labelModifierFn = (label)=>label.split(':')[1]
-    this.categories$ = this.quizService.getAllCategories().pipe(shareReplay(1));
-    this.mainCategories$ = this.categories$.pipe(
-      map(categories =>
-        categories.map(category => category.name.split(':')[0])
-          .filter((value, index, self) => self.indexOf(value) === index)
-          .map(mainCategoryName => ({
-            id: categories.find(category => category.name.startsWith(mainCategoryName))?.id || 0,
-            name: mainCategoryName
-          }))
-      )
-    );
-  }
-  onMainCategoryChange(dropdownOption: DropdownOption ) {
-    this.showSubCategories = false;
-    this.subCategories$.next([]);
-    this.changeDetectorRef.detectChanges();
-    this.categories$?.pipe(
-      map(categories => ({
-        mainCategory: categories.find(category => category.name.split(':')[0] === dropdownOption.label), // todo redundant`
-        subCategories: categories.filter(category => category.name.startsWith(dropdownOption.label + ':'))
-      }))
-    ).subscribe(({mainCategory, subCategories}) => {
-      this.showSubCategories=false
-      this.selectedCategoryId = mainCategory?.id; // todo redundant`
-      this.showSubCategories = subCategories.length > 0;
-      this.subCategories$.next(subCategories);
-
-    });
-  }
-
-
-  onSubCategoryChange(dropdownOption: DropdownOption  ) {
-    this.selectedCategoryId = dropdownOption.value;
-  }
-
-  onDifficultyChange(dropdownOption: DropdownOption ){
-    this.selectedDifficulty = dropdownOption.value as Difficulty;
-  }
-
-  createQuiz() {
-    if (this.selectedCategoryId && this.selectedDifficulty) {
-      this.questions$ = this.quizService.createQuiz(this.selectedCategoryId.toString(), this.selectedDifficulty); // typing todo ls
+    constructor(private quizService: QuizService, private changeDetectorRef: ChangeDetectorRef) {
     }
-  }
 
-  getCategoriesAsDropdownElement(categories: Category[]): DropdownOption[] {
-   return categories.map(c => ({ value: c.id, label: c.name })) || []
-  }
+    ngOnInit() {
+        this.labelModifierFn = (label) => label.split(':')[1]
+        this.categories$ = this.quizService.getAllCategories().pipe(shareReplay(1));
+        this.mainCategories$ = this.categories$.pipe(
+            map(categories =>
+                categories.map(category => category.name.split(':')[0])
+                    .filter((value, index, self) => self.indexOf(value) === index)
+                    .map(mainCategoryName => ({
+                        id: categories.find(category => category.name.startsWith(mainCategoryName))?.id || 0,
+                        name: mainCategoryName
+                    }))
+            )
+        );
+    }
+
+    onMainCategoryChange(dropdownOption: DropdownOption) {
+        this.showSubCategories = false;
+        this.subCategories$.next([]);
+        this.changeDetectorRef.detectChanges();
+        this.categories$?.pipe(
+            map(categories => ({
+                mainCategory: categories.find(category => category.name.split(':')[0] === dropdownOption.label), // todo redundant`
+                subCategories: categories.filter(category => category.name.startsWith(dropdownOption.label + ':'))
+            }))
+        ).subscribe(({mainCategory, subCategories}) => {
+            this.showSubCategories = false
+            this.selectedCategoryId = mainCategory?.id; // todo redundant`
+            this.showSubCategories = subCategories.length > 0;
+            this.subCategories$.next(subCategories);
+
+        });
+    }
+
+
+    onSubCategoryChange(dropdownOption: DropdownOption) {
+        this.selectedCategoryId = dropdownOption.value;
+    }
+
+    onDifficultyChange(dropdownOption: DropdownOption) {
+        this.selectedDifficulty = dropdownOption.value as Difficulty;
+    }
+
+    createQuiz() {
+        if (this.selectedCategoryId && this.selectedDifficulty) {
+            this.questions$ = this.quizService.createQuiz(this.selectedCategoryId.toString(), this.selectedDifficulty); // typing todo ls
+        }
+    }
+
+    getCategoriesAsDropdownElement(categories: Category[]): DropdownOption[] {
+        return categories.map(c => ({value: c.id, label: c.name})) || []
+    }
 }
 
 
